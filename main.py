cb347caf89b686fdcd737f428bde0f14c3b053b5
reza-rezvani2052 <reza.rezvani2052@gmail.com>
2025-04-30 03:45:47 +0330

    add SplashScreen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-
-from mainwindow import MainWindow
-from PySide6.QtWidgets import QApplication
+# import time
+from PySide6.QtWidgets import QApplication, QSplashScreen
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -9,7 +10,25 @@ if __name__ == "__main__":
     # from PySide6.QtCore import Qt
     # app.setLayoutDirection(Qt.RightToLeft)
 
+    # ...
+
+    splash_pix = QPixmap("splash.png")
+    splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+    splash.setWindowFlag(Qt.FramelessWindowHint)
+    splash.show()
+
+    #  اجازه بده کمی سیستم پردازش کنه
+    app.processEvents()
+
+    # time.sleep(3)
+
+    # ...
+    from mainwindow import MainWindow
+
     window = MainWindow()
     window.show()
+    # ...
+
+    splash.finish(window)
 
     sys.exit(app.exec())
